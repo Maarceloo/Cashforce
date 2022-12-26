4d1b26512a71c313699ba9b811bf29105077f5bb
Marcelo De Lima <delima_marcelo@outlook.com>
2022-12-26 20:27:14 -0300

    Feat: OFFERS migrations/models

---
+++ src/database/models/offers.js
@@ -0,0 +1,67 @@
+const OffersSchema = (sequelize, DataTypes) => {
+  const OffersTable = sequelize.define(
+    "offers",
+    {
+      id: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+      },
+      tax: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      tariff: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      adValorem: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      float: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      iof: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      expiresIn: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      paymentStatusSponsor: {
+        defaultValue: 0,
+        type: DataTypes.TINYINT,
+      },
+      paymentStatusProvider: {
+        defaultValue: 0,
+        type: DataTypes.TINYINT,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      orderId: {
+        defaultValue: null,
+        type: DataTypes.INTEGER,
+      },
+      sponsorId: {
+        defaultValue: null,
+        type: DataTypes.INTEGER,
+      },
+    },
+    {
+      tableName: "offers",
+      underscored: true,
+      timestamps: false,
+    }
+  );
+  return OffersTable;
+};
+
+module.exports = OffersSchema;
