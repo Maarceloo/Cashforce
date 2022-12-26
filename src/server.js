e04c64956aeaa00f27c6cfec6e1da38dfcd988f1
Marcelo De Lima <delima_marcelo@outlook.com>
2022-12-26 20:21:14 -0300

    fix

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 const app = require('./app');
 const PORT = 3001
 
-app.listen(PORT, () => console.log('server running on port 3001'));
+app.listen(PORT, async () => {
+  console.log(`server running on port ${PORT}`);
+});
