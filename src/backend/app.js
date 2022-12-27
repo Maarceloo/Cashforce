85f6a8b5a460c342aa5a4a239ab428d3c314d195
Marcelo De Lima <delima_marcelo@outlook.com>
2022-12-27 19:12:23 -0300

    feat: arquivo de backend criado

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const invoicesRoute = require('./Routes/Invoices')
 
 const app = express();
 
 app.use(express.json());
+app.use('/invoices', invoicesRoute)
 
 module.exports = app;
