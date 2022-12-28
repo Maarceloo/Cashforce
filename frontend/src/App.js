dbbe371a9181c84d4cc074a24a0684d92ec9c284
Marcelo De Lima <delima_marcelo@outlook.com>
2022-12-28 14:10:19 -0300

    feat: configurando app e index

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import "./css/App.css";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Invoices from "./pages/Invoices";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Invoices />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
