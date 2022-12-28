5480d3dad3f1f1b70f48a9007c9430aba7d04743
Marcelo De Lima <delima_marcelo@outlook.com>
2022-12-28 14:07:45 -0300

    fix: instalando e implementando o cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,11 @@
-const express = require('express');
-const invoicesRoute = require('./Routes/Invoices')
+const express = require("express");
+const cors = require("cors");
+const invoicesRoute = require("./Routes/Invoices");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-app.use('/invoices', invoicesRoute)
+app.use("/invoices", invoicesRoute);
 
 module.exports = app;
